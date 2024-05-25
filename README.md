5f79dcaf2df15b6d766934b3195b09517c392f40
kusumpreetheer <kusumpreetheer@gmail.com>
2024-05-25 16:43:02 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# References
+
+- https://dribbble.com/shots/2325967-K
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Header from './components/Header'
+
+const App = () => {
+
+  return (
+    <>
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+        <Header />
+      </div>
+    </>
+  )
+}
+
+export default App
